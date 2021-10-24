79ba4a9f9e0dea77e58b75231c3bc87eef963911
brobeson <brobeson@users.noreply.github.com>
2021-10-24 12:30:44 -0600

    fix a minor bug in loading a dataset

--- tracking-analyzer/dataset.cpp
+++ tracking-analyzer/dataset.cpp
@@ -15,9 +15,7 @@ namespace analyzer
 
     auto read_sequences(const QString& dataset_path)
     {
-      const QDir directory {dataset_path};
-      const auto names {
-        directory.entryList(QDir::Dirs | QDir::NoDotAndDotDot, QDir::Name)};
+      const auto names {read_sequence_names(dataset_path)};
       QVector<analyzer::sequence> sequences;
       sequences.reserve(names.size());
       for (const auto& name : names)
@@ -134,7 +132,6 @@ namespace analyzer
   auto load_dataset(const QString& path) -> analyzer::dataset
   {
     const auto dataset_path {analyzer::make_absolute_path(path)};
-    const auto sequences {analyzer::read_sequence_names(dataset_path)};
     return analyzer::dataset {dataset_path,
                               analyzer::read_sequences(dataset_path)};
   }
